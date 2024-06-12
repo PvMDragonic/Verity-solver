c6e0f50a70a07a62b17ab8cf407cb44ecbef4d96
João Pedro Droval <55903030+PvMDragonic@users.noreply.github.com>
2024-06-12 14:51:24 -0300

    Mobile responsiveness fix.
    Had to add some extra containers which bloated the JSX and SASS,
    but oh well.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -22,47 +22,53 @@ export function Home()
     return (
         <div className = 'home'>
             <section className = 'home__container'>
-                <p className = 'home__text'>Inside</p>
-                <div className = 'home__symbols'>
-                    <InsideSymbol
-                        pos = {0}
-                        label = {'Left'}
-                        symbols = {insideSymbols}
-                        setSymbols = {setInsideSymbols}
-                    />
-                    <InsideSymbol
-                        pos = {1}
-                        label = {'Middle'}
-                        symbols = {insideSymbols}
-                        setSymbols = {setInsideSymbols}
-                    />
-                    <InsideSymbol
-                        pos = {2}
-                        label = {'Right'}
-                        symbols = {insideSymbols}
-                        setSymbols = {setInsideSymbols}
-                    />
-                </div>
-                <p className = 'home__text'>Outside</p>
-                <div className = 'home__symbols'>
-                    <OutsideSymbol
-                        pos = {0}
-                        label = {'Left'}
-                        symbols = {outsideSymbols}
-                        setSymbols = {setOutsideSymbols}
-                    />
-                    <OutsideSymbol
-                        pos = {1}
-                        label = {'Middle'}
-                        symbols = {outsideSymbols}
-                        setSymbols = {setOutsideSymbols}
-                    />
-                    <OutsideSymbol
-                        pos = {2}
-                        label = {'Right'}
-                        symbols = {outsideSymbols}
-                        setSymbols = {setOutsideSymbols}
-                    />
+                <div className = 'home__subcontainer'>
+                    <div className = 'home__symbols-container'>
+                        <p className = 'home__text'>Inside</p>
+                        <div className = 'home__symbols'>
+                            <InsideSymbol
+                                pos = {0}
+                                label = {'Left'}
+                                symbols = {insideSymbols}
+                                setSymbols = {setInsideSymbols}
+                            />
+                            <InsideSymbol
+                                pos = {1}
+                                label = {'Middle'}
+                                symbols = {insideSymbols}
+                                setSymbols = {setInsideSymbols}
+                            />
+                            <InsideSymbol
+                                pos = {2}
+                                label = {'Right'}
+                                symbols = {insideSymbols}
+                                setSymbols = {setInsideSymbols}
+                            />
+                        </div>
+                    </div>
+                    <div className = 'home__symbols-container'>
+                        <p className = 'home__text'>Outside</p>
+                        <div className = 'home__symbols'>
+                            <OutsideSymbol
+                                pos = {0}
+                                label = {'Left'}
+                                symbols = {outsideSymbols}
+                                setSymbols = {setOutsideSymbols}
+                            />
+                            <OutsideSymbol
+                                pos = {1}
+                                label = {'Middle'}
+                                symbols = {outsideSymbols}
+                                setSymbols = {setOutsideSymbols}
+                            />
+                            <OutsideSymbol
+                                pos = {2}
+                                label = {'Right'}
+                                symbols = {outsideSymbols}
+                                setSymbols = {setOutsideSymbols}
+                            />
+                        </div>
+                    </div>
                 </div>
                 <p className = 'home__text'>Solution</p>
                 <div className = 'home__result'>
