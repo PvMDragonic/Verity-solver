56f107bdbc46d215671e176f0cd022cf9610e348
João Pedro Droval <55903030+PvMDragonic@users.noreply.github.com>
2024-06-17 10:15:43 -0300

    Language buttons shrink for mobile.
    Simple eventListener to watch the view's size and set the JSX.
    
    Didn't use a ResizeObserver because this was more concise and
    to-the-point, even tho manipulating the DOM directly isn't React's
    way of handling things.

--- src/pages/Home/options.tsx
+++ src/pages/Home/options.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 export function Options()
 {
+    const [mobile, setMobile] = useState<boolean>(false);
     const { t, i18n } = useTranslation();
+
+    useEffect(() => 
+    {
+        // Sets on initial load.
+        setMobile(window.innerWidth < 550);
+
+        const handleResize = () => setMobile(window.innerWidth < 550);
+        window.addEventListener('resize', handleResize); 
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
 
     return (
         <div className = 'options'>
@@ -14,13 +25,13 @@ export function Options()
                         className = 'options__language-button'
                         onClick = {() => i18n.changeLanguage('en')}
                     >
-                        English
+                        {mobile ? 'EN' : 'English'}
                     </button>
                     <button 
                         className = 'options__language-button'
                         onClick = {() => i18n.changeLanguage('pt')}
                     >
-                        Português
+                        {mobile ? 'PT' : 'Português'}
                     </button>
                 </div>
             </div>
