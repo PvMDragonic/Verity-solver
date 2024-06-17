4e44b70bbe504172bab7fb8b4cc210e4ccf4ef82
João Pedro Droval <55903030+PvMDragonic@users.noreply.github.com>
2024-06-17 08:23:39 -0300

    Options button and interface.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,107 +1,28 @@
-import React, { useEffect, useState } from "react";
-import { InsideSymbol } from "../../components/InsideSymbol";
-import { OutsideSymbol } from "../../components/OutsideSymbol";
-import { PuzzleSolver } from "./solver";
-import { Results } from "../../components/Results";
+import React, { useState } from "react";
+import { MainContent } from "./mainContent";
+import { Options } from "./options";
+import Gear from "../../assets/Gear";
 import '../../scss/main.scss';
 
 export function Home()
 {
-    const [insideSymbols, setInsideSymbols] = useState<string[]>(['', '', '']);
-    const [outsideSymbols, setOutsideSymbols] = useState<string[]>(['', '', '']);
-    const [instructions, setInstructions] = useState<string[][]>([]);
-    const [finalSymbols, setFinalSymbols] = useState<React.FC[]>([]);
-    const [resultMessage, setResultMessage] = useState<string>('');
-    
-    useEffect(() => 
-    {
-        if (insideSymbols.some(str => str === '') || outsideSymbols.some(str => str === ''))
-        {
-            setResultMessage('Please select all symbols.');
-            return;
-        } 
-
-        const { finalSymbols, instructions } = PuzzleSolver(insideSymbols, outsideSymbols);
-
-        // Checks to see if theres a Cube, Sphere or Tetraheron among the returned.
-        if (finalSymbols === undefined)
-        {
-            setResultMessage('Impossible combination set; try again.');
-            return;
-        }
-
-        setInstructions(instructions);
-        setFinalSymbols(finalSymbols);
-        setResultMessage('');
-
-    }, [insideSymbols, outsideSymbols]);
+    const [showOptions, setShowOptions] = useState<boolean>(false);
 
     return (
         <div className = 'home'>
             <section className = 'home__container'>
-                <div className = 'home__subcontainer'>
-                    <div className = 'home__symbols-container'>
-                        <p className = 'home__text'>Inside</p>
-                        <div className = 'home__symbols'>
-                            <InsideSymbol
-                                pos = {0}
-                                label = {'Left'}
-                                symbols = {insideSymbols}
-                                setSymbols = {setInsideSymbols}
-                            />
-                            <InsideSymbol
-                                pos = {1}
-                                label = {'Middle'}
-                                symbols = {insideSymbols}
-                                setSymbols = {setInsideSymbols}
-                            />
-                            <InsideSymbol
-                                pos = {2}
-                                label = {'Right'}
-                                symbols = {insideSymbols}
-                                setSymbols = {setInsideSymbols}
-                            />
-                        </div>
-                    </div>
-                    <div className = 'home__symbols-container'>
-                        <p className = 'home__text'>Outside</p>
-                        <div className = 'home__symbols'>
-                            <OutsideSymbol
-                                pos = {0}
-                                label = {'Left'}
-                                symbols = {outsideSymbols}
-                                setSymbols = {setOutsideSymbols}
-                            />
-                            <OutsideSymbol
-                                pos = {1}
-                                label = {'Middle'}
-                                symbols = {outsideSymbols}
-                                setSymbols = {setOutsideSymbols}
-                            />
-                            <OutsideSymbol
-                                pos = {2}
-                                label = {'Right'}
-                                symbols = {outsideSymbols}
-                                setSymbols = {setOutsideSymbols}
-                            />
-                        </div>
-                    </div>
-                </div>
-                <p className = 'home__text'>Solution</p>
-                {resultMessage !== '' ? (
-                    <div 
-                        className = 'home__results'
-                        style = {{ height: '100%' }}
-                    >
-                        <p>{resultMessage}</p>
-                    </div>
+                <button
+                    className = 'home__options-button'
+                    style = {{ opacity: showOptions ? '70%' : '10%' }}
+                    onClick = {() => setShowOptions(curr => !curr)}
+                >
+                    <Gear/>
+                </button>
+
+                {!showOptions ? (
+                    <MainContent/>
                 ) : (
-                    <div className = 'home__results'>
-                        <Results
-                            finalSymbols = {finalSymbols}
-                            instructions = {instructions}
-                        />
-                    </div>
+                    <Options/>
                 )}
             </section>
             <p className = 'home__credits'>Made by Dragonic#2329 — View project on <a href='https://github.com/PvMDragonic/Verity-solver'>Github</a>.</p>
---
+++ src/pages/Home/mainContent.tsx
@@ -0,0 +1,107 @@
+import React, { useEffect, useState } from "react";
+import { InsideSymbol } from "../../components/InsideSymbol";
+import { OutsideSymbol } from "../../components/OutsideSymbol";
+import { PuzzleSolver } from "./solver";
+import { Results } from "../../components/Results";
+import '../../scss/main.scss';
+
+export function MainContent()
+{
+    const [insideSymbols, setInsideSymbols] = useState<string[]>(['', '', '']);
+    const [outsideSymbols, setOutsideSymbols] = useState<string[]>(['', '', '']);
+    const [instructions, setInstructions] = useState<string[][]>([]);
+    const [finalSymbols, setFinalSymbols] = useState<React.FC[]>([]);
+    const [resultMessage, setResultMessage] = useState<string>('');
+    
+    useEffect(() => 
+    {
+        if (insideSymbols.some(str => str === '') || outsideSymbols.some(str => str === ''))
+        {
+            setResultMessage('Please select all symbols.');
+            return;
+        } 
+
+        const { finalSymbols, instructions } = PuzzleSolver(insideSymbols, outsideSymbols);
+
+        // Checks to see if theres a Cube, Sphere or Tetraheron among the returned.
+        if (finalSymbols === undefined)
+        {
+            setResultMessage('Impossible combination set; try again.');
+            return;
+        }
+
+        setInstructions(instructions);
+        setFinalSymbols(finalSymbols);
+        setResultMessage('');
+
+    }, [insideSymbols, outsideSymbols]);
+
+    return (
+        <>
+            <div className = 'home__subcontainer'>
+                <div className = 'home__symbols-container'>
+                    <p className = 'home__text'>Inside</p>
+                    <div className = 'home__symbols'>
+                        <InsideSymbol
+                            pos = {0}
+                            label = {'Left'}
+                            symbols = {insideSymbols}
+                            setSymbols = {setInsideSymbols}
+                        />
+                        <InsideSymbol
+                            pos = {1}
+                            label = {'Middle'}
+                            symbols = {insideSymbols}
+                            setSymbols = {setInsideSymbols}
+                        />
+                        <InsideSymbol
+                            pos = {2}
+                            label = {'Right'}
+                            symbols = {insideSymbols}
+                            setSymbols = {setInsideSymbols}
+                        />
+                    </div>
+                </div>
+                <div className = 'home__symbols-container'>
+                    <p className = 'home__text'>Outside</p>
+                    <div className = 'home__symbols'>
+                        <OutsideSymbol
+                            pos = {0}
+                            label = {'Left'}
+                            symbols = {outsideSymbols}
+                            setSymbols = {setOutsideSymbols}
+                        />
+                        <OutsideSymbol
+                            pos = {1}
+                            label = {'Middle'}
+                            symbols = {outsideSymbols}
+                            setSymbols = {setOutsideSymbols}
+                        />
+                        <OutsideSymbol
+                            pos = {2}
+                            label = {'Right'}
+                            symbols = {outsideSymbols}
+                            setSymbols = {setOutsideSymbols}
+                        />
+                    </div>
+                </div>
+            </div>
+            <p className = 'home__text'>Solution</p>
+            {resultMessage !== '' ? (
+                <div 
+                    className = 'home__results'
+                    style = {{ height: '100%' }}
+                >
+                    <p>{resultMessage}</p>
+                </div>
+            ) : (
+                <div className = 'home__results'>
+                    <Results
+                        finalSymbols = {finalSymbols}
+                        instructions = {instructions}
+                    />
+                </div>
+            )}
+        </>
+    )
+}
